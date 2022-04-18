af06a51c36a50acb4cd50a1392278ee3d3f04ec2
Aref111n <71724445+Aref111n@users.noreply.github.com>
2022-04-18 14:28:35 +0600

    Create adding-icon.dart

---
+++ adding-icon.dart
@@ -0,0 +1,15 @@
+// To add app icon, first download the pic you want to add as app icon
+// Go to this link: https://appicon.co/
+// Drop your image, unselect ipad, watchOS, MacOS and keep only iPhone and Android
+// Click on Generate, it will download a Zip file in your PC.
+// Unzip that file, you will find two file: "res" and "Assets.xcassets"
+// Open the res file, there you will find 5 mipmap files which are actually 5 different resolutations of the icon.
+// Go to the directories of android studio, under the project name, there are two folder "Android" & "ios" 
+// Open in this way: Android/app/src/main/res/
+// Opn res with explorar.
+// You will find the similar named files as you founded in your downloaded res file.
+// Delete the five mipmap files named files from the PC's res. Copy them from your downloaded file and paste to your PC's res file
+// Then to set for iPhone, go through: ios/runner/ 
+// you will see the file Assets.xcassets file. replace it with the downloaded Assets.xcassets file by above process.
+// This is all done, run your project and see the change
+  
