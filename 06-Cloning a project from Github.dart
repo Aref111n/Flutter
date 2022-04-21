e5c0f58e2d75da9610fef6e5e539f70a54d756e8
Aref111n <71724445+Aref111n@users.noreply.github.com>
2022-04-21 12:09:50 +0600

    Create 06-Cloning a project from Github.dart

---
+++ 06-Cloning a project from Github.dart
@@ -0,0 +1,8 @@
+// Copy the link of that project from the 'code' option of github
+// From your android studio, go to file > new > project from version control
+// Version control will be 'Git' in the dropdown option menues.
+// On the URL box, paste the link
+// Select the directory where the project will be saved.
+// Click on 'Clone' and it will automaticaly download the file for you on the pc.
+// A pop-up notification will ask If we want to open the project now. Select No.
+// Go to file > open and open that project, or go to 'open an existing project' .
