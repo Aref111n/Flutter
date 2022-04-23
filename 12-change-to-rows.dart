9d76d6c82300d2ca465d92d718f5a1e12fe378df
Aref111n <71724445+Aref111n@users.noreply.github.com>
2022-04-24 02:52:20 +0600

    Create 12-change-to-rows.dart

---
+++ 12-change-to-rows.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            //mainAxisSize: MainAxisSize.min,
+            //verticalDirection: VerticalDirection.up,
+            //mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                //margin: EdgeInsets.all(20.0),
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                color: Colors.blue,
+                child: Text('Welcome'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                //margin: EdgeInsets.all(20.0),
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                color: Colors.red,
+                child: Text('To'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                //margin: EdgeInsets.all(20.0),
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                color: Colors.yellow,
+                child: Text('Basic'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
